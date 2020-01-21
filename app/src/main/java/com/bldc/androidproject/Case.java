f82e16ee575e8dd46facb84329d559fc550ede9f
BastienLeduc <bastien_l@hotmail.fr>
2020-01-21 11:29:48 +0100

    play rule with bug out of band pl.114

--- app/src/main/java/com/bldc/androidproject/Case.java
+++ app/src/main/java/com/bldc/androidproject/Case.java
@@ -6,10 +6,39 @@ import android.widget.ImageButton;
 public class Case extends android.support.v7.widget.AppCompatImageButton {
 
     private Boolean state;
+    private Boolean use;
 
-    public Case(Context context, boolean state) {
+    public Case(Context context, boolean state, boolean use) {
         super(context);
-        this.state = state;
+        setState(state);
+        setUse(use);
+    }
+
+    public Boolean getUse() {
+        return use;
+    }
+
+    public void setUse(Boolean use) {
+        this.use = use;
+        if (use) {
+            this.setBackgroundColor(this.getContext().getResources().getColor(R.color.colorPrimaryDark));
+            this.setActivated(true);
+        } else {
+            this.setBackgroundColor(this.getContext().getResources().getColor(R.color.colorBack));
+            this.setActivated(false);
+        }
+
+    }
+
+    @Override
+    public void setActivated(boolean activated) {
+        super.setActivated(activated);
+        if (activated) {
+            this.setBackgroundColor(this.getContext().getResources().getColor(R.color.colorPrimaryDark));
+            this.state = false;
+        } else {
+            this.setBackgroundColor(this.getContext().getResources().getColor(R.color.colorBack));
+        }
     }
 
     public Boolean getState() {
@@ -18,5 +47,9 @@ public class Case extends android.support.v7.widget.AppCompatImageButton {
 
     public void setState(Boolean state) {
         this.state = state;
+        if (state)
+            this.setBackgroundColor(this.getContext().getResources().getColor(R.color.colorPrimary));
+        else
+            this.setBackgroundColor(this.getContext().getResources().getColor(R.color.colorPrimaryDark));
     }
 }
--- app/src/main/java/com/bldc/androidproject/Plateau.java
+++ app/src/main/java/com/bldc/androidproject/Plateau.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 @SuppressLint("ValidFragment")
@@ -28,17 +29,17 @@ public class Plateau extends Fragment {
     private final ArrayList<Integer> noneActivCase = new ArrayList<Integer>(Arrays.asList(listValue));
     private int nbCoups;
     private int nbBilles;
-    private ImageButton[][] tabIm;
+    private Case[][] tabIm;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_plateau, container, false);
         plateau = (GridLayout) view.findViewById(R.id.plateau);
         textViewScore = (TextView) view.findViewById(R.id.textViewScore);
-        createCase();
         nbCoups = 0;
         nbBilles = 0;
-        tabIm = new ImageButton[nbLigneCol][nbLigneCol];
+        tabIm = new Case[nbLigneCol][nbLigneCol];
+        createCase();
         return view;
     }
 
@@ -51,7 +52,12 @@ public class Plateau extends Fragment {
             for (int j = 0; j < nbLigneCol; j++) {
                 if (noneActivCase.contains(i) && noneActivCase.contains(j)) {
                 } else {
-                    final Case imB = new Case(getContext(), false);
+                    Case imB = null;
+                    if (i == Math.round(nbLigneCol / 2) && j == Math.round(nbLigneCol / 2)) {
+                        imB = new Case(getContext(), false, false);
+                    } else {
+                        imB = new Case(getContext(), false, true);
+                    }
                     GridLayout.LayoutParams params = new GridLayout.LayoutParams();
                     params.height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, Objects.requireNonNull(getContext()).getResources().getDisplayMetrics());
                     params.width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, Objects.requireNonNull(getContext()).getResources().getDisplayMetrics());
@@ -67,24 +73,24 @@ public class Plateau extends Fragment {
                     imB.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
-                            imB.setBackgroundColor(view.getContext().getResources().getColor(R.color.colorPrimary));
-                            imB.setState(false);
-                            for (int i = 0; i < plateau.getChildCount(); i++) {
-                                if (verif(plateau, plateau.getChildAt(i))) {
+                            ArrayList<Case> lCaseSt = verif(plateau, view);
+
+                            if (lCaseSt != null) {
+                                for (Case c : lCaseSt) {
+                                    playOneBall(plateau, (int) c.getX(), (int) c.getY(), finalI, finalJ);
                                 }
+                            } else {
+                                Case t = (Case) view;
+                                t.setState(true);
                             }
-                            for (int i = 0; i < plateau.getChildCount(); i++) {
-                                if (plateau.getChildAt(i).isSelected() && imB.isSelected()) {
+
+                            /*for (int i = 0; i < plateau.getChildCount(); i++) {
+                                if (plateau.getChildAt(i).isSelected() && finalImB.isSelected()) {
                                 }
-                            }
+                            }*/
+
                         }
                     });
-                    if (i == Math.round(nbLigneCol / 2) && j == Math.round(nbLigneCol / 2)) {
-                        imB.setBackgroundColor(this.getContext().getResources().getColor(R.color.colorBack));
-                    } else {
-                        imB.setBackgroundColor(this.getContext().getResources().getColor(R.color.colorPrimaryDark));
-
-                    }
                     tabIm[i][j] = imB;
                     plateau.addView(imB);
                 }
@@ -92,41 +98,59 @@ public class Plateau extends Fragment {
         }
     }
 
-    public boolean verif(GridLayout plat, View v) {
+    public ArrayList<Case> verif(GridLayout plat, View v) {
+        ArrayList<Case> lCase = new ArrayList<Case>();
         for (int i = 0; i < plat.getChildCount(); i++) {
-            if (tabIm[(int) plat.getChildAt(i).getX()][(int) plat.getChildAt(i).getY() + 2] != null) {
-                Case c = (Case) tabIm[(int) plat.getChildAt(i).getX()][(int) plat.getChildAt(i).getY() + 2];
-                if (c.getState() == true) {
+            if (((int)plat.getChildAt(i).getY() + 2) < nbLigneCol) {
+                if (tabIm[(int) plat.getChildAt(i).getX()][(int) plat.getChildAt(i).getY() + 2] != null) {
+                    Case c = (Case) tabIm[(int) plat.getChildAt(i).getX()][(int) plat.getChildAt(i).getY() + 2];
+                    if (c != null && c.getState() == true) {
+                        lCase.add(c);
+
+                    }
                 }
             }
-            if (tabIm[(int) plat.getChildAt(i).getX() + 2][(int) plat.getChildAt(i).getY()] != null) {
-                Case c = (Case) tabIm[(int) plat.getChildAt(i).getX() + 2][(int) plat.getChildAt(i).getY() + 2];
-                if (c.getState() == true) {
+            if (((int)plat.getChildAt(i).getX() + 2) < nbLigneCol) {
+                if (tabIm[(int) plat.getChildAt(i).getX() + 2][(int) plat.getChildAt(i).getY()] != null) {
+                    Case c = (Case) tabIm[(int) plat.getChildAt(i).getX() + 2][(int) plat.getChildAt(i).getY()];
+                    if (c != null && c.getState() == true) {
+                        lCase.add(c);
+
+                    }
                 }
             }
-            if (tabIm[(int) plat.getChildAt(i).getX()-2][(int) plat.getChildAt(i).getY()] != null) {
-                Case c = (Case) tabIm[(int) plat.getChildAt(i).getX()-2][(int) plat.getChildAt(i).getY()];
-                if (c.getState() == true) {
+            if (plat.getChildAt(i).getX() - 2 > 0) {
+                if (tabIm[(int) plat.getChildAt(i).getX() - 2][(int) plat.getChildAt(i).getY()] != null) {
+                    Case c = (Case) tabIm[(int) plat.getChildAt(i).getX() - 2][(int) plat.getChildAt(i).getY()];
+                    if (c != null && c.getState() == true) {
+                        lCase.add(c);
+
+                    }
                 }
             }
-            if (tabIm[(int) plat.getChildAt(i).getX()][(int) plat.getChildAt(i).getY() + 2] != null) {
-                Case c = (Case) tabIm[(int) plat.getChildAt(i).getX()][(int) plat.getChildAt(i).getY() + 2];
-                if (c.getState() == true) {
+            if (plat.getChildAt(i).getY() - 2 > 0) {
+                if (tabIm[(int) plat.getChildAt(i).getX()][(int) plat.getChildAt(i).getY() - 2] != null) {
+                    Case c = (Case) tabIm[(int) plat.getChildAt(i).getX()][(int) plat.getChildAt(i).getY() - 2];
+                    if (c != null && c.getState() == true) {
+                        lCase.add(c);
+
+                    }
                 }
             }
-            if ((plat.getRowCount() < plat.getChildAt(i).getY() + 2 && plat.getColumnCount() < plat.getChildAt(i).getX()) &&
+            /*if ((plat.getRowCount() < plat.getChildAt(i).getY() + 2 && plat.getColumnCount() < plat.getChildAt(i).getX()) &&
                     (plat.getRowCount() < plat.getChildAt(i).getY() - 2 && plat.getColumnCount() < plat.getChildAt(i).getX()) &&
                     (plat.getRowCount() < plat.getChildAt(i).getY() && plat.getColumnCount() < plat.getChildAt(i).getX() + 2) &&
                     (plat.getRowCount() < plat.getChildAt(i).getY() && plat.getColumnCount() < plat.getChildAt(i).getX() - 2)) {
 
-            }
+            }*/
         }
-        if ((plat.getRowCount() < v.getY() + 2 && plat.getColumnCount() < v.getX()) &&
+        /*if ((plat.getRowCount() < v.getY() + 2 && plat.getColumnCount() < v.getX()) &&
                 (plat.getRowCount() < v.getY() - 2 && plat.getColumnCount() < v.getX()) &&
                 (plat.getRowCount() < v.getY() && plat.getColumnCount() < v.getX() + 2) &&
                 (plat.getRowCount() < v.getY() && plat.getColumnCount() < v.getX() - 2)) {
             return true;
-        } else return false;
+        } else return false;*/
+        return lCase;
 
     }
 
@@ -136,25 +160,26 @@ public class Plateau extends Fragment {
         textViewScore.setText("Nb billes restantes : " + nbBilles);
     }
 
-    private boolean playOneBall(GridLayout plat, int x1, int y1, int x2, int y2) {
+    private void playOneBall(GridLayout plat, int x1, int y1, int x2, int y2) {
         int valMidX = (x1 + x2) / 2;
         int valMidY = (y1 + y2) / 2;
-        if ((x1 == x2 && Math.abs(y1 - y2) == 2) || (y1 == y2 && Math.abs(x1 - x2) == 2)) {
+        if ((x1 == x2 && Math.abs(y1 - y2) == 2) || (y1 == y2 && Math.abs(x1 - x2) == 2))
             for (int i = 0; i < plat.getChildCount(); i++) {
+                if (plat.getChildAt(i).getX() == valMidX && plat.getChildAt(i).getY() == valMidY) {
+                    if (!plat.getChildAt(i).isActivated()) {
+                        nbCoups++;
+                        nbBilles--;
+                    }
+                }
                 if (plat.getChildAt(i).getX() == x1 && plat.getChildAt(i).getY() == y1) {
-                    plat.getChildAt(i).setActivated(false);
+                    if (plat.getChildAt(i).isActivated()) plat.getChildAt(i).setActivated(false);
                 }
                 if (plat.getChildAt(i).getX() == x2 && plat.getChildAt(i).getY() == y2) {
-                    plat.getChildAt(i).setActivated(false);
+                    if (plat.getChildAt(i).isActivated()) plat.getChildAt(i).setActivated(true);
                 }
-                if (plat.getChildAt(i).getX() == valMidX && plat.getChildAt(i).getY() == valMidY) {
-                    plat.getChildAt(i).setBackgroundColor(this.getContext().getResources().getColor(R.color.colorPrimary));
-                }
+
             }
-            nbCoups++;
-            nbBilles--;
-            return true;
-        } else return false;
+
     }
 }
 
