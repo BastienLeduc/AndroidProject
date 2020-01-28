9653ca75e288309b5660102e93db3145647b9d97
unknown <matth.more@lilo.org>
2020-01-28 14:37:28 +0100

    music + sound

--- app/src/main/java/com/bldc/androidproject/MenuActivity.java
+++ app/src/main/java/com/bldc/androidproject/MenuActivity.java
@@ -1,6 +1,8 @@
 package com.bldc.androidproject;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,9 @@ public class MenuActivity extends AppCompatActivity {
 
     private Button btnPlay = null;
     private Button btnScore = null;
+    private MediaPlayer pressBtn;
+    private MediaPlayer music;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,32 @@ public class MenuActivity extends AppCompatActivity {
 
         btnPlay = (Button) findViewById(R.id.btn_play);
         btnScore = (Button) findViewById(R.id.btn_scoreFromMenu);
+
+        music = (MediaPlayer) MediaPlayer.create(this ,R.raw.menumusic);
+        music.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        music.start();
+                    }
+                });
+            }
+        });
+        music.setLooping(true);
+    }
+
+    private void playPress(){
+        pressBtn = (MediaPlayer) MediaPlayer.create(this ,R.raw.menusound);
+        pressBtn.start();
+        pressBtn.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                pressBtn.stop();
+                pressBtn.release();
+            }
+        });
     }
 
     @Override
@@ -27,6 +58,7 @@ public class MenuActivity extends AppCompatActivity {
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                playPress();
                 final Intent mainActivityIntent = new Intent(MenuActivity.this, MainActivity.class);
                 startActivity(mainActivityIntent);
 
@@ -36,10 +68,33 @@ public class MenuActivity extends AppCompatActivity {
         btnScore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                playPress();
                 final Intent mainActivityIntent = new Intent(MenuActivity.this, ScoreList.class);
                 startActivity(mainActivityIntent);
 
             }
         });
+        music.start();
+    }
+
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        music.start();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        music.pause();
+        //music.release();
+
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        music.stop();
+        music.release();
     }
 }
